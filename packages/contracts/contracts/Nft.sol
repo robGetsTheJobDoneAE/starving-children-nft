2871848fc0caaa04390c4a246f673433b8ecfa65
Robert Seares <robertseares@Roberts-MacBook-Pro-2.local>
2022-07-15 11:00:11 -0400

    add listner

--- packages/contracts/contracts/Nft.sol
+++ packages/contracts/contracts/Nft.sol
@@ -5,9 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721, Ownable {
+contract StavingChildrenNft is ERC721, Ownable {
     using Counters for Counters.Counter;
-
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("MyToken", "MTK") {}
